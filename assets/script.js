32f64b510def8d59ee313bee1880bd47c64cdd04
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-09-20 23:01:48 -0400

    stuck for a few hours. error 401

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,32 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+const express = require("express");
+const app = express();
+const https = require("https");
+var city = "toronto";
+var apiKey = "4b2fa07fb7030ef840b93e9c3ec4f585";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&appid=" + apiKey;
+
+    app.get("/", function (req, res) {
+        https.get(weatherUrl, function (response) {
+            console.log(response.statusCode);
+
+            response.on("data", function (data) {
+                var currentWeatherData = JSON.parse(data)
+                var currentCityName = currentWeatherData.name;
+          
+
+                console.log(currentCityName);
+            })
+        })
+        res.send("hi");
+    })
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.");
+})
