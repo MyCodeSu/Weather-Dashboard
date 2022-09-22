91cbd25ed72cc9f02b4f1475773bd89ff443a47e
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-09-21 23:52:03 -0400

    restarting vscode

--- assets/app.js
+++ assets/app.js
@@ -18,78 +18,78 @@ var formSubmissionHandler = document.querySelector("#submit").addEventListener("
     var cityName = formSubmission.value.trim();
 
 
-    if (cityName) {
-        getCurrentCityData(cityName);
-        formSubmission.value = "";
-    } else {
-        alert("Please enter a city name");
-    };
+//     if (cityName) {
+//         getCurrentCityData(cityName);
+//         formSubmission.value = "";
+//     } else {
+//         alert("Please enter a city name");
+//     };
 })
 
 
-// function to fetch current location specific data.
-var getCurrentCityData = function (city) {
-    var weatherCurrentURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=" + city + "&appid=" + apiKey;
-    fetch(weatherCurrentURL).then(response => {
-        if (response.ok) {
-            response.json().then(function (data) {
-                genCurrentWeatherData(data);
-            })
-        } else {
-            alert("Please enter a city name.");
-        }
-    })
-        .catch(error => {
-            alert(error);
-        });
-};
+// // function to fetch current location specific data.
+// var getCurrentCityData = function (city) {
+//     var weatherCurrentURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=" + city + "&appid=" + apiKey;
+//     fetch(weatherCurrentURL).then(response => {
+//         if (response.ok) {
+//             response.json().then(function (data) {
+//                 genCurrentWeatherData(data);
+//             })
+//         } else {
+//             alert("Please enter a city name.");
+//         }
+//     })
+//         .catch(error => {
+//             alert(error);
+//         });
+// };
 
-// current date weather content
-function genCurrentWeatherData(data) {
-var cityNameDisplay = data.name;
-const currentDate = new Date();
-var currentIcon = data.weather[0].icon;
-var currentIconImg = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png";
-var currentWeatherDescription = data.weather[0].description;
-var currentTemp = data.main.temp;
-var currentWind = "Wind speed: " + data.wind.speed + " m/s";
-var currentHumid = data.main.humidity;
+// // current date weather content
+// function genCurrentWeatherData(data) {
+// var cityNameDisplay = data.name;
+// const currentDate = new Date();
+// var currentIcon = data.weather[0].icon;
+// var currentIconImg = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png";
+// var currentWeatherDescription = data.weather[0].description;
+// var currentTemp = data.main.temp;
+// var currentWind = "Wind speed: " + data.wind.speed + " m/s";
+// var currentHumid = data.main.humidity;
 
-document.createElement("h1").innerText = cityNameDisplay+"   "+currentDate;
-var imgEl = document.createElement("img");
-imgEl.setAttribute("src", currentIconImg);
-imgEl.setAttribute("alt", currentWeatherDescription);
-
-
-}
-
-// get current weather data
-// var currentData = function (data) {
- 
-//     genCurrentWeatherData(data);
-
+// document.createElement("h1").innerText = cityNameDisplay+"   "+currentDate;
+// var imgEl = document.createElement("img");
+// imgEl.setAttribute("src", currentIconImg);
+// imgEl.setAttribute("alt", currentWeatherDescription);
 
 
 // }
 
+// // get current weather data
+// // var currentData = function (data) {
+ 
+// //     genCurrentWeatherData(data);
 
-// function to fetch future location specific data.
 
-var getFutureCityData = function (city) {
-    var weatherForecastURL = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=" + city + "&appid=" + apiKey;
-    fetch(weatherUrl).then(response => {
-        if (response.ok) {
-            response.json().then(function (data) {
 
-            })
-        } else {
-            alert("Please enter a city name.");
-        }
-    })
-        .catch(error => {
-            alert("Cannot connect to Open Weather. Please try again.");
-        });
-};
+// // }
+
+
+// // function to fetch future location specific data.
+
+// var getFutureCityData = function (city) {
+//     var weatherForecastURL = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=" + city + "&appid=" + apiKey;
+//     fetch(weatherUrl).then(response => {
+//         if (response.ok) {
+//             response.json().then(function (data) {
+
+//             })
+//         } else {
+//             alert("Please enter a city name.");
+//         }
+//     })
+//         .catch(error => {
+//             alert("Cannot connect to Open Weather. Please try again.");
+//         });
+// };
 
 // city name
 
@@ -102,4 +102,3 @@ var getFutureCityData = function (city) {
 // wind
 
 // humidity
-
