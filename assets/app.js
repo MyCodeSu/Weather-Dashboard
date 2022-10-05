c9dc97a473ebf7bcd0b8d4747cc1895529e72ee6
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-05 06:10:35 -0400

    double checked to confirm all acceptance criteria have been met.

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// THEN I am presented with current and future conditions for that city and that city is added to the search history            DONE
 // WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed    DONE
 // WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity    DONE
 // WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// THEN I am again presented with current and future conditions for that city       DONE
 
 
 const apiKey = "4b2fa07fb7030ef840b93e9c3ec4f585";
